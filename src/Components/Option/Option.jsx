88daa22c8038c529e6d379da986d915e8a9c8989
Abdulmojeed <mojeedayoola58@gmail.com>
2023-01-23 12:50:06 +0100

    Added Option component

--- src/Components/Option/Option.jsx
+++ src/Components/Option/Option.jsx
@@ -1,20 +1,48 @@
 import React from "react";
-import OptionCard from "./OptionCard";
-import photo1 from '../../Images/unsplash_nEtpvJjnPVo.png'
-import photo2 from '../../Images/unsplash_oGmf8o53LeE.png'
-import photo3 from '../../Images/unsplash_uooMllXe6gE.png'
-import photo4 from '../../Images/unsplash_XdWkFaHI97c.png'
+import classes from './Option.module.css';
+import photo1 from '../../Images/unsplash_nEtpvJjnPVo.png';
+import photo2 from '../../Images/Image 2.png';
+import photo3 from '../../Images/unsplash_uooMllXe6gE.png';
+import photo4 from '../../Images/unsplash_XdWkFaHI97c.png';
 
 const Option = () => {
 
     return (
-        <div>
-            <div className="optionCard">
-                <OptionCard title='flexible leases' src={photo1} />
-                <OptionCard title='flexible leases' src={photo2} />
-                <OptionCard title='flexible leases' src={photo3} />
-                <OptionCard title='flexible leases' src={photo4} />
+        <div className={classes.option}>
+            <div className={classes.optionCard}>
+                    <div className={classes.flex1}>
+                        <div>
+                            <h2>flexible leases</h2>
+                            <img src={photo1} alt='flexible leases' />
+                        </div>
+
+                        <div>
+                            <h2>7-Day happiness guaranteed</h2>
+                            <img src={photo2} alt='flexible leases' />
+                        </div>
+                    </div>
+
+                    <div className={classes.flex2}>
+                        <div>
+                            <h2>choose your own roommate</h2>
+                            <img src={photo4} alt='flexible leases' />
+                        </div>
+
+                        <div>
+                            <h2>Monthly house cleaning</h2>
+                            <img src={photo3} alt='flexible leases' />
+                        </div>
+                    </div>
             </div>
+
+            <div className={classes.optionDescription}>
+                <h1>Flexibility and options to suit your lifestyle.</h1>
+                <p>You need it? We got it. We make finding your next home easy, comfortable, and simple. From our happiness guarantee to our selective roommate finder option. We provide you the flexibility that you most desire.</p>
+                <button className={classes.optionBtn}>
+                    Search Rooms
+                </button>
+            </div>
+
         </div>
     )
 }
